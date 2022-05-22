dceef4893f77eb6a894b3a6567150163efa962ac
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-05-22 16:27:28 +0430

    change btn banner color

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -14,33 +14,26 @@ const Banner = ({
   imageUrl,
 }) => {
   return (
-    <Flex
-      flexWrap={"wrap"}
-      justifyContent="center"
-      alignItems={"center"}
-      m="10"
-    >
-      <Image src={imageUrl} width={500} height={500} alt="banner" />
-
-      <Box p={5}>
-        <Text color={"gray.500"} fontSize="sm" fontWeight={"medium"}>
+    <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+      <Image src={imageUrl} width={500} height={300} alt="banner" />
+      <Box p="5">
+        <Text color="gray.500" fontSize="sm" fontWeight="medium">
           {purpose}
         </Text>
-        <Text fontSize="3x1" fontWeight={"bold"}>
-          {title1} <br /> {title2}
+        <Text fontSize="3xl" fontWeight="bold">
+          {title1}
+          <br />
+          {title2}
         </Text>
-        <Text
-          color={"gray.700"}
-          fontSize="lg"
-          fontWeight={"medium"}
-          paddingBottom={"3"}
-          paddingTop="e"
-        >
-          {desc1} <br /> {desc2}
+        <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">
+          {desc1}
+          <br />
+          {desc2}
         </Text>
-
-        <Button fontSize={"xl"}>
-          <Link href={linkName}>{buttonText}</Link>
+        <Button fontSize="xl" bg="blue.300">
+          <Link href={linkName}>
+            <a>{buttonText}</a>
+          </Link>
         </Button>
       </Box>
     </Flex>
