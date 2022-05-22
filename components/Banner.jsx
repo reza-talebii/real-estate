8a28605880eeaa8779237f165645536e6ea82d75
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-05-22 16:07:56 +0430

    complete Banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,7 +1,50 @@
+import Link from "next/link";
+import Image from "next/image";
+
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
-const Banner = () => {
-  return <div>Banner</div>;
+const Banner = ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  buttonText,
+  linkName,
+  imageUrl,
+}) => {
+  return (
+    <Flex
+      flexWrap={"wrap"}
+      justifyContent="center"
+      alignItems={"center"}
+      m="10"
+    >
+      <Image src={imageUrl} width={500} height={500} alt="banner" />
+
+      <Box p={5}>
+        <Text color={"gray.500"} fontSize="sm" fontWeight={"medium"}>
+          {purpose}
+        </Text>
+        <Text fontSize="3x1" fontWeight={"bold"}>
+          {purpose}
+        </Text>
+        <Text
+          color={"gray.700"}
+          fontSize="lg"
+          fontWeight={"medium"}
+          paddingBottom={"3"}
+          paddingTop="e"
+        >
+          {purpose}
+        </Text>
+
+        <Button fontSize={"xl"}>
+          <Link href={linkName}>{buttonText}</Link>
+        </Button>
+      </Box>
+    </Flex>
+  );
 };
 
 export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-import Link from "next/link";
-import Image from "next/image";
+import { Banner } from "../components";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Banner purpose={"for sale"} />
+    </>
+  );
 }
