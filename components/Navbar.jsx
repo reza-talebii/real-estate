3013703ef04f29ddc9d68ea39d320785769abf76
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-05-23 17:13:28 +0430

    complete navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import Link from "next/link";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  Flex,
+  Box,
+  Spacer,
+} from "@chakra-ui/react";
+
+import { FcMenu, FcHome, FcAbout } from "react-icons/fc";
+import { BsSearch } from "react-icons/bs";
+import { FiKey } from "react-icons/fi";
+
+const Navbar = () => (
+  <Flex p="2" borderBottom={"1px"} borderColor="gray.100">
+    <Box fontSize={"3xl"} color="blue.400" fontWeight={"bold"}>
+      <Link href={"/"}>RealTor</Link>
+    </Box>
+    <Spacer />
+
+    <Box>
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          icon={<FcMenu />}
+          variant="outlined"
+          color="red.400"
+        />
+
+        <MenuList>
+          <Link href={"/"} passHref>
+            <MenuItem icon={<FcHome />}>Home</MenuItem>
+          </Link>
+        </MenuList>
+        <MenuList>
+          <Link href={"/search"} passHref>
+            <MenuItem icon={<BsSearch />}>Search</MenuItem>
+          </Link>
+        </MenuList>
+        <MenuList>
+          <Link href={"/search?purpose=for-sale"} passHref>
+            <MenuItem icon={<FcAbout />}>Sale Property</MenuItem>
+          </Link>
+        </MenuList>
+        <MenuList>
+          <Link href={"//search?purpose=for-rent"} passHref>
+            <MenuItem icon={<FiKey />}>Rent Property</MenuItem>
+          </Link>
+        </MenuList>
+      </Menu>
+    </Box>
+  </Flex>
+);
 
 export default Navbar;
