c668e193186f262f698a97e2fc466b34281f80d7
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-05-24 11:12:43 +0430

    fix build bug

--- components/ImageScrollBar.jsx
+++ components/ImageScrollBar.jsx
@@ -43,7 +43,13 @@ export default function ImageSrollbar({ data }) {
       style={{ overflow: "hidden" }}
     >
       {data.map((item) => (
-        <Box width="910px" itemId={item.id} overflow="hidden" p="1">
+        <Box
+          width="910px"
+          itemId={item.id}
+          overflow="hidden"
+          p="1"
+          key={item.id}
+        >
           <Image
             placeholder="blur"
             blurDataURL={item.url}
@@ -51,6 +57,7 @@ export default function ImageSrollbar({ data }) {
             width={1000}
             height={500}
             sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
+            alt={"house"}
           />
         </Box>
       ))}
