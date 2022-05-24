af65eea1a7075d275eecad94632bd84b4c9616d0
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-05-24 09:48:29 +0430

    complete filter search

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -15,11 +15,47 @@ import {
 
 import { MdCancel } from "react-icons/md";
 
+import { filterData, getFilterValues } from "../utils/filterData";
+
 const SearchFilter = () => {
-  const [filters, setFilters] = useState({});
+  const [filters, setFilters] = useState(filterData);
+  const router = useRouter();
+
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+
+    const values = getFilterValues(filterValues);
+
+    values.map((item) => {
+      query[item.name] = item.value;
+    });
+
+    router.push({ pathname: path, query });
+  };
 
   return (
-    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap"></Flex>
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters?.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+            cursor={"pointer"}
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
   );
 };
 
